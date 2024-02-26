973741443736409c8ab26f5dc65d26746bb41c2b
mohammad_mursalin <mursalinlamon@gmail.com>
2024-02-26 15:00:17 +0600

    Teacher class for school management system

---
+++ Teacher.java
@@ -0,0 +1,47 @@
+public class Teacher {
+    
+    private int id;
+    private String name;
+    private int totalSalary;
+    private int salaryPaid = 0;
+
+    Teacher(int id, int salary, String name) {
+
+        this.id = id;
+        this.name = name;
+        this.totalSalary = salary;
+
+    }
+
+    public String getName() {
+
+        return name;
+
+    }
+
+    public int getId() {
+
+        return id;
+
+    }
+
+    public int getTotalSalary() {
+
+        return totalSalary;
+
+    }
+
+    public int getSalaryPaid() {
+
+        return salaryPaid;
+
+    }
+
+    public int remainingSalary() {
+
+        return totalSalary - salaryPaid;
+        
+    }
+
+
+}
